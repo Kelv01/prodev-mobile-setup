09bdad1e2da1cace88ce065c369bad83e29cbdea
kelv01 <kelvinnjugunanungari@gmail.com>
2026-01-11 21:18:11 +0300

    modified x03 app/index.tsx

--- prodev-mobile-app-0x03/app/index.tsx
+++ prodev-mobile-app-0x03/app/index.tsx
@@ -14,9 +14,7 @@ export default function Index() {
 
         <Text style={styles.largeText}>Sign in to your</Text>
         <Text style={styles.largeText}>Account</Text>
-        <Text style={styles.smallText}>
-          Enter your email and password to sign in.
-        </Text>
+        <Text style={styles.smallText}>Enter your email and password to sign in.</Text>
 
         <View style={styles.formGroup}>
           <View>
